13f6a1fa9ee1e3ab4a6eeae1fe65b2f869850008
kirbyismygod <105167935+kirbyismygod@users.noreply.github.com>
2022-08-21 14:33:18 +0100

    Add files via upload

---
+++ scp containment breach (1.18.2)/scpcontainmentbreach/src/main/java/com/mossgod/scpcontainmentbreach/Scpcontainmentbreach.java
@@ -0,0 +1,18 @@
+package com.mossgod.scpcontainmentbreach;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("scp_containment_breach")
+public class Scpcontainmentbreach {
+	
+	
+	
+	
+	
+	
+	public scp_containment_breach() {
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+}
