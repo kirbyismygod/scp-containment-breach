aded016a94e8496f76d0f11a1561e95ea31f9662
44759 <thisisno420@outlook.com>
2022-08-26 16:35:52 +0100

    added monitor file was going to work on it but had some things to do first

---
+++ scp containment breach (1.18.2)/scpcontainmentbreach/src/main/java/com/mossgod/scp/init/custom/blocks/monitor.java
@@ -0,0 +1,13 @@
+package com.mossgod.scp.init.custom.blocks;
+
+
+import net.minecraft.world.level.block.Block;
+
+public class monitor extends Block {
+    public monitor(Properties p_49795_) {
+        super(p_49795_);
+    }
+
+
+
+}
